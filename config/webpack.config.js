57c9593625f23b43b5def90f2cbf3307f4035cf1
springer <fcfight@163.com>
2016-11-15 17:32:25 +0800

    webpackconfig

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,34 +1,44 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var contextPath = path.resolve(__dirname, '..', 'src');
+var outputPath = path.resolve(__dirname, '..', 'dist');
 
 
 module.exports = {
 
     //Default: process.cwd(), 返回当前进程工作所在的目录
-    context: __dirname + "./src",
+    context: contextPath,
 
     //入口文件,可以是string、array或者是object，可以设置多个入口文件，
     //以键值对的形式
-    entry: "./src/app.js",
+    entry: "./src/entry.js",
 
     //出口文件
     output: {
       //定义打包输出文件的路径，注意：可以有多个entry的值，但是只有一个指定的output配置
-      path: __dirname + "/dist",
-      filename: "bundle.js"
+      path: outputPath,
+      filename: "js/bundle.js"
       // publicPath:
     },
     module: {
       loaders:[
         {
-          test:/\.js$/,
+          test:/\.js?$/,
           include: [
             path.resolve(__dirname, "..", "src")
           ],
           loaders: [
             'babel'
           ]
+        },
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract("style-loader","css-loader")
         }
       ]
+    },
+    resolve: {
+
     }
 };
