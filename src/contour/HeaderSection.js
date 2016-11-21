fe4c755d00a52c644b4a27fd89aaea308662312c
springer <fcfight@163.com>
2016-11-21 17:56:29 +0800

    commit src

--- src/contour/HeaderSection.js
+++ src/contour/HeaderSection.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class HeaderSection extends Component {
   render() {
     return(
-      
+      <header class="nav-header">
+        <ul class="nav-blog">
+          <li><a href="#">Docs</a></li>
+          <li><a href="#">Support</a></li>
+          <li><a href="#">Download</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </header>
     );
   }
 }
