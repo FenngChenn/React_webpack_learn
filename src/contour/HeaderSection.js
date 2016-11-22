658f82581e0d6e4f49ea62e5817c2b474a88cd23
springer <fcfight@163.com>
2016-11-22 18:01:27 +0800

    commit contour

--- src/contour/HeaderSection.js
+++ src/contour/HeaderSection.js
@@ -4,6 +4,7 @@ class HeaderSection extends Component {
   render() {
     return(
       <header class="nav-header">
+        <HeadImage />
         <ul class="nav-blog">
           <li><a href="#">Docs</a></li>
           <li><a href="#">Support</a></li>
@@ -11,6 +12,14 @@ class HeaderSection extends Component {
           <li><a href="#">Blog</a></li>
         </ul>
       </header>
+    );
+  }
+}
+
+class HeadImage extends Component {
+  render() {
+    return(
+      <img src="img/head.jpg" class="head-animate">
     );
   }
 }
