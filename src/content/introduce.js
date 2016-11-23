1a227e7ad6a64eab8d7ab7d9fffb801de87aaea9
springer <fcfight@163.com>
2016-11-23 17:58:22 +0800

    update content

---
+++ src/content/introduce.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class IntroduceSelf extends Component {
+  render() {
+    return(
+      <div class="introduce">
+        <div>front-end girl and lucky girl</div>
+        <div>hobby</div>
+        <div>company</div>
+        <div>school</div>
+      </div>
+    );
+  }
+}
+
+module.exports = {
+  IntroduceSelf
+}
