e506b2675882e518f01fb1f8a8c37a7f04c46233
springer <fcfight@163.com>
2016-11-24 16:29:02 +0800

    update contour

--- src/contour/ContentSection.js
+++ src/contour/ContentSection.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import { IntroduceSelf } from './../content/IntroduceSelf.js';
+import { BlogSection } from './../content/BlogSection.js';
 
 class ContentSection extends Component {
   render() {
     return (
       <section>
+        <IntroduceSelf />
+        <BlogSection />
       </section>
     );
   }
+}
+
+module.exports = {
+  ContentSection
 }
