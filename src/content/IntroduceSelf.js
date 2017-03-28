dbd6530f7b7a46830779edc986f1f471195fee32
springer <fcfight@163.com>
2017-03-28 17:55:54 +0800

    0328 src

--- src/content/IntroduceSelf.js
+++ src/content/IntroduceSelf.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 
 class IntroduceSelf extends Component {
+  // constructor(props)
+  getInitialState: function(){
+    return{userInput: ''};
+  },
+  handleChange: function(e){
+    this.setState({userInput: e.target.value});
+  },
   render() {
     return(
       <div className="introduce">
+        <label htmlFor=""></label>
+        <input type="text" ref="myInput" value={this.state.userInput} onChange={this.handleChange} />
         <div>front-end girl and lucky girl</div>
         <div>hobby</div>
         <div>company</div>
